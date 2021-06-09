77df6cc2ced8f494b0fed4fa44ce43c4d961acbe
Augusto Lima <augustolimads@gmail.com>
2021-06-09 00:00:56 -0300

    first commit

---
+++ src/screens/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+// import * as S from './styles';
+
+export const Main = () => {
+  return (
+    <View>
+      <Text>oi mundo</Text>
+    </View>
+  );
+};
