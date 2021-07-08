7db3d577fd0d6d88b8f1a85215f31d007bb8080d
Augusto Lima <augustolimads@gmail.com>
2021-07-07 23:57:51 -0300

    fix path imports

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -3,17 +3,14 @@ import { Container } from "src/components/Container";
 import { Text } from "src/components/Text";
 import { Center } from "src/components/Center";
 import { Caption } from "src/components/Caption";
-import { H3 } from "src/components/H3";
-import { H2 } from "src/components/H2";
 import { H1 } from "src/components/H1";
-
 // import * as S from './styles';
 
 export const Main = () => {
   return (
     <Container>
       <Center>
-        <H1>Home page</H1>
+        <H1>Home Page</H1>
         <Text>Testando o texto...</Text>
       </Center>
     </Container>
